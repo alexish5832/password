327f61890eb84f47d32eb312746d629988d1684b
alexish5832 <144606285+alexish5832@users.noreply.github.com>
2023-10-01 17:17:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-111
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def adios(ctx, count_adios = 5):
+    await ctx.send("adios_amigo" * count_heh)
+
+bot.run("MTE1ODAxOTQ2MTM3MTEzNDA5Mw.Gq1E5b.sSe7Aa0JDMYTbgn34BFZ3BNLb7OstTsIlhIiRY")
